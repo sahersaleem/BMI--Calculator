522589a542053589fe1dd7e1a09ef4faabf4b678
Muhammad Saad <160317082+sahersaleem@users.noreply.github.com>
2024-03-16 19:06:03 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,53 @@
+import inquirer from "inquirer";
+
+
+async function bmiCalc() {
+   console.log("Welcome to the BMI calculator app ! ")
+   const answers = await inquirer.prompt([{
+      type: "number",
+      name: "height",
+      message: "Please enter your height in feet : "
+   },
+   {
+      type: "number",
+      name: "weight",
+      message: "Please enter your weight in kg  : "
+
+   }
+
+
+
+   ])
+   const { height, weight } = answers
+   const height1 =(answers.height * 0.3048 )**2
+
+
+   let calcBmi = (weight / height1)
+   let round = Math.floor(calcBmi)
+   console.log(`Your BMI is ${round}`)
+
+   if (calcBmi < 18.5) {
+      console.log("you are weak")
+   }
+   else if (calcBmi > 18.5 && calcBmi < 25) {
+      console.log("You are healthy!")
+
+   }
+   else if (calcBmi > 25 && calcBmi < 30) {
+      console.log("You are overweight")
+   }
+
+   else {
+      console.log("You are obese!")
+   }
+
+}
+do {
+   const wait = await bmiCalc()
+   var again = await inquirer.prompt({
+      type: "input",
+      name: "again",
+      message: "Do you want to continue press Y : "
+
+   })
+} while (again.again == "Y" || again.again == "Yes" || again.again == "yes" || again.again == "y" || again.again == "YES")
